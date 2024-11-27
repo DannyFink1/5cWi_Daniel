2da26ad90e75fc65e869ed9da8546fb26d507327
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2024-11-27 12:56:39 +0100

    added DataGenerator AlgoDat

---
+++ src/at/fid/AlgoDat/DataGenerator.java
@@ -0,0 +1,32 @@
+package at.fid.AlgoDat;
+
+import java.util.Random;
+
+public class DataGenerator {
+
+    public static int[] generateDataArray(int size) {
+        Random random = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = random.nextInt();
+        }
+        return data;
+    }
+
+    public static int[] generateDataArray(int size, int min, int max) {
+        Random random = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = random.nextInt(min, max);
+        }
+        return data;
+    }
+
+    public static void printDataArray(int[] data) {
+        System.out.println("---- Array ----");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+        System.out.println("---------------");
+    }
+}
---
+++ src/at/fid/AlgoDat/Test.java
@@ -0,0 +1,11 @@
+package at.fid.AlgoDat;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] dataTest = DataGenerator.generateDataArray(5);
+        int[] dataTest2 = DataGenerator.generateDataArray(5, 1, 5);
+        DataGenerator.printDataArray(dataTest);
+        DataGenerator.printDataArray(dataTest2);
+    }
+
+}
