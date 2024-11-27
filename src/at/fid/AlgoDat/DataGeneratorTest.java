9b075b745ece395ac653703f41dda80b1571ce62
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2024-11-27 13:03:18 +0100

    added TestClass

---
+++ src/at/fid/AlgoDat/DataGeneratorTest.java
@@ -0,0 +1,23 @@
+package at.fid.AlgoDat;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class DataGeneratorTest {
+    @Test
+    void testGenerateDataArray() {
+        int[] dataTest = DataGenerator.generateDataArray(5);
+        Assert.assertEquals(dataTest.length, 5);
+    }
+
+    @Test
+    void testGenerateDataArray2() {
+        int[] dataTest2 = DataGenerator.generateDataArray(5, 1, 5);
+        Assert.assertEquals(dataTest2.length, 5);
+    }
+
+    @Test
+    void testPrintDataArray() {
+
+    }
+}
