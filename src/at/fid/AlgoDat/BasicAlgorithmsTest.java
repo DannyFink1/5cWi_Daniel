b0f523ab3b7f019075dd2828ab4a6c7236cdd0a9
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2024-12-04 12:44:15 +0100

    added Tests

---
+++ src/at/fid/AlgoDat/BasicAlgorithmsTest.java
@@ -0,0 +1,28 @@
+package at.fid.AlgoDat;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class BasicAlgorithmsTest {
+    @Test
+    void testMax() {
+        int[] data = { 0, -5, 2, 3, 4 };
+        int max = BasicAlgorithms.max(data);
+        Assert.assertEquals(4, max);
+    }
+
+    @Test
+    void testMin() {
+        int[] data = { 0, -5, 2, 3, 4 };
+        int min = BasicAlgorithms.min(data);
+        Assert.assertEquals(-5, min);
+    }
+
+    @Test
+    void testRevert() {
+        int[] data = { 0, -5, 2, 3, 4 };
+        int[] revertTest = { 4, 3, 2, -5, 0 };
+        int[] newData = BasicAlgorithms.revert(data);
+        Assert.assertArrayEquals(revertTest, newData);
+    }
+}
