9be286fdb1dfa751e2d4ccaf7e04e24ed67f952b
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2024-12-04 12:39:48 +0100

    Added min max algorithms

--- src/at/fid/AlgoDat/BasicAlgorithms.java
+++ src/at/fid/AlgoDat/BasicAlgorithms.java
@@ -13,4 +13,27 @@ public class BasicAlgorithms {
         }
         return newData;
     }
+
+    public static int max(int[] data) {
+
+        int max = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] data) {
+
+        int min = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+        return min;
+    }
+
 }
--- src/at/fid/AlgoDat/Test.java
+++ src/at/fid/AlgoDat/Test.java
@@ -3,9 +3,12 @@ package at.fid.AlgoDat;
 public class Test {
     public static void main(String[] args) {
         int[] dataTest = DataGenerator.generateDataArray(5);
-        int[] dataTest2 = DataGenerator.generateDataArray(5, 1, 5);
+        int[] dataTest2 = DataGenerator.generateDataArray(5, -4, -1);
         DataGenerator.printDataArray(dataTest2);
         DataGenerator.printDataArray(BasicAlgorithms.revert(dataTest2));
+        System.out.println(BasicAlgorithms.max(dataTest2));
+        System.out.println(BasicAlgorithms.min(dataTest2));
+
     }
 
 }
